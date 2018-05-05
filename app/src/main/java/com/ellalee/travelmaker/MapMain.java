b6b10070ddbdf990328935775cd2ba7a24478d77
eunji.ella <tyu5412@gmail.com>
2018-05-05 22:22:38 +0900

    add placePicker

--- app/src/main/java/com/ellalee/travelmaker/MapMain.java
+++ app/src/main/java/com/ellalee/travelmaker/MapMain.java
@@ -1,28 +1,25 @@
 package com.ellalee.travelmaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.FragmentManager;
-
 import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
-
 import com.google.android.gms.maps.GoogleMap;
-
 import com.google.android.gms.maps.MapFragment;
-
 import com.google.android.gms.maps.OnMapReadyCallback;
-
 import com.google.android.gms.maps.model.LatLng;
-
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 
 public class MapMain extends AppCompatActivity implements OnMapReadyCallback {
+    int PLACE_PICKER_REQUEST = 1;
 
     @Override
 
@@ -33,31 +30,45 @@ public class MapMain extends AppCompatActivity implements OnMapReadyCallback {
         setContentView(R.layout.activity_map_main);
 
 
+        callPlacePicker();
+/*
         FragmentManager fragmentManager = getFragmentManager();
 
         MapFragment mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
-
+*/
     }
 
+    void callPlacePicker(){
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try{
+            startActivityForResult(builder.build(this),PLACE_PICKER_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PLACE_PICKER_REQUEST){
+            if(resultCode == RESULT_OK){
+                Place place = PlacePicker.getPlace(data,this);
+                String msg = String.format("Place: %s",place.getName());
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
+    @Override
     public void onMapReady(final GoogleMap map) {
-
-
         LatLng SEOUL = new LatLng(37.56, 126.97);
 
 
         MarkerOptions markerOptions = new MarkerOptions();
-
         markerOptions.position(SEOUL);
-
         markerOptions.title("서울");
-
         markerOptions.snippet("한국의 수도");
-
         map.addMarker(markerOptions);
 
 
