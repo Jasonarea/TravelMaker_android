7c1fc614ec745dbf0a9d660cb1156d7afd8bad54
Jason Jung <hyeonsuns123@gmail.com>
2018-05-10 23:22:40 +0900

    Gmail Inbox Synchronization ver.1.0

---
+++ app/src/main/java/com/ellalee/travelmaker/InfoActivity.java
@@ -0,0 +1,38 @@
+package com.ellalee.travelmaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/**
+ * Created by User on 4/29/2015.
+ */
+public class InfoActivity extends Activity {
+
+    TextView header;
+    TextView body;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+        header = (TextView)findViewById(R.id.header);
+        body = (TextView)findViewById(R.id.body);
+        String s = "";
+        String b = "";
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            b = extras.getString("body");
+            s = extras.getString("subject");
+        }
+
+        body.setText(b);
+        header.setText(s);
+    }
+}
