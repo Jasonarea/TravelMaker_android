81ab1fee2c87677f641ce0571f476ae30caa4909
AnneJiwon <jiwoni108@naver.com>
2018-05-18 04:22:11 +0900

    main and menu UI update

--- app/src/main/java/com/ellalee/travelmaker/MainActivity.java
+++ app/src/main/java/com/ellalee/travelmaker/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void menuMain(View v) {
+        startActivity(new Intent(getApplicationContext(), MenuMain.class));
+}
+
+    public void calenderMain(View v) {
+        startActivity(new Intent(getApplicationContext(), CalendarMain.class));
     }
    /* public void mapMain(View v){
         startActivity(new Intent(getApplicationContext(),MapMain.class));
